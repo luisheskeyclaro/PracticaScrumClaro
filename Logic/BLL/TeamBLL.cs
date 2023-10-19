82a2f4f104218d66a70a3b22f77927d45bdb0ef0
DilsonZapata <elyi0596@gmail.com>
2023-10-19 13:39:22 -0400

    add create team

---
+++ Logic/BLL/TeamBLL.cs
@@ -0,0 +1,31 @@
+﻿using Logic.Models;
+using PracticaScrumClaro.DataManager;
+using PracticaScrumClaro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Logic.BLL
+
+{
+    public class TeamBLL
+    {
+        public ResponseData Save(Team data)
+        {
+            try
+            {
+                DataManager.SaveData<Team>(data, "Team.txt");
+                return new ResponseData { IsSuccess = true, Message = "Guardado Correctamente." };
+
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData { IsSuccess = false, Message = ex.Message };
+            }
+        }
+    }
+}
---
+++ Logic/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public Member(string name, string role)
+        {
+            Name = name;
+            Role = role;
+        }
+
+    }
+}
