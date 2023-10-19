b0dae69ea7102f74509841571fee621c23ccb2f5
Gabriel Santana <Gabriel Santana@LT-21198>
2023-10-19 13:44:13 -0400

    Creando Unit Test Class para ProyectoBLL

---
+++ PracticaScrumClaro.Test/ProyectoBLL_Facts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+public sealed class ProyectoBLL_Facts
+{
+	[TestCase("Proyecto1.txt")]
+	public void SaveData_WithFileNotCreated (string fileName)
+	{
+        ProyectoBLL
+        Assert.That()
+	}
+}
